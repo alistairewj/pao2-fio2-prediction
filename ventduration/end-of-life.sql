0388115ce3d5062d80757002ef76ba35b1e17935
Tom Pollard <tpollard@mit.edu>
2017-12-12 16:07:20 -0500

    add trach details

--- ventduration/end-of-life.sql
+++ ventduration/end-of-life.sql
@@ -14,6 +14,12 @@ WITH status AS (
                ELSE NULL END AS endoflife_offset,
            CASE WHEN c.cplitemvalue LIKE 'Terminal%' THEN c.cplitemoffset
                ELSE NULL END AS terminal_offset,
+           CASE WHEN c.cplitemvalue LIKE 'Bedside tracheotomy%' THEN c.cplitemoffset
+               ELSE NULL END AS bedside_trach_offset,
+           CASE WHEN c.cplitemvalue LIKE 'Intubated/trach-acute%' THEN c.cplitemoffset
+               ELSE NULL END AS trach_acute_offset,
+           CASE WHEN c.cplitemvalue LIKE 'Intubated/trach-chronic%' THEN c.cplitemoffset
+               ELSE NULL END AS trach_chronic_offset,
            CASE WHEN c.cplitemvalue LIKE 'Wears glasses%' THEN 'yeah'
                ELSE '' END AS wears_glasses
     FROM careplangeneral c
@@ -21,8 +27,13 @@ WITH status AS (
         OR c.cplitemvalue LIKE 'Comfort measures only%'
         OR c.cplitemvalue LIKE 'End of life%'
         OR c.cplitemvalue LIKE 'Terminal%'
+        OR c.cplitemvalue LIKE 'Bedside tracheotomy%'
+        OR c.cplitemvalue LIKE 'Intubated/trach-acute%'
+        OR c.cplitemvalue LIKE 'Intubated/trach-chronic%'
         OR c.cplitemvalue LIKE 'Wears glasses%'
+
   )
 SELECT s.patientunitstayid, s.dnr_offset, s.cmo_offset, s.endoflife_offset, 
-       s.terminal_offset, s.wears_glasses
+       s.terminal_offset, s.bedside_trach_offset, s.trach_acute_offset,
+       s.trach_chronic_offset, s.wears_glasses
 FROM status s;
