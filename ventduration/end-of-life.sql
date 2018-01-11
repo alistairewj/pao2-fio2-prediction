cc5cc397c1000d94c9822638928ea75c524f3735
Tom Pollard <tpollard@mit.edu>
2018-01-11 12:58:18 +0000

    rm glasses

--- ventduration/end-of-life.sql
+++ ventduration/end-of-life.sql
@@ -20,8 +20,6 @@ WITH status AS (
                ELSE NULL END AS trach_acute_offset,
            CASE WHEN c.cplitemvalue LIKE 'Intubated/trach-chronic%' THEN c.cplitemoffset
                ELSE NULL END AS trach_chronic_offset,
-           CASE WHEN c.cplitemvalue LIKE 'Wears glasses%' THEN 'yeah'
-               ELSE '' END AS wears_glasses
     FROM careplangeneral c
     WHERE c.cplitemvalue LIKE 'Do not resuscitate%'
         OR c.cplitemvalue LIKE 'Comfort measures only%'
@@ -30,8 +28,6 @@ WITH status AS (
         OR c.cplitemvalue LIKE 'Bedside tracheotomy%'
         OR c.cplitemvalue LIKE 'Intubated/trach-acute%'
         OR c.cplitemvalue LIKE 'Intubated/trach-chronic%'
-        OR c.cplitemvalue LIKE 'Wears glasses%'
-
   )
 SELECT s.patientunitstayid, s.dnr_offset, s.cmo_offset, s.endoflife_offset, 
        s.terminal_offset, s.bedside_trach_offset, s.trach_acute_offset,
