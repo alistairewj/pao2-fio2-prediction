02327343ca68deb8ca03a870ed2a770976f1a1e8
Alistair Johnson <alistairewj@gmail.com>
2018-02-01 18:57:23 -0500

    add pao2/fio2 to table

--- sql/pao2fio2.sql
+++ sql/pao2fio2.sql
@@ -1,18 +1,29 @@
 DROP TABLE IF EXISTS pf_pao2fio2;
 CREATE TABLE pf_pao2fio2 AS
-select lab.patientunitstayid
-  , lab.labresultoffset
-  , min(case when labname = 'paO2' then labresult else null end) as PaO2
-  , min(case when labname = 'FiO2' then
-          -- input validation
-          case
-            when labresult < 0.209 then null
-            when labresult <= 1.0 then labresult*100.0
-            when labresult < 20.9 then null
-            when labresult > 100 then null
-          else labresult end
-        else null end) as FiO2
-from lab
-where labname in ('paO2','FiO2')
-and labresult > 0
-group by lab.patientunitstayid, lab.labresultoffset;
+with t1 as
+(
+  select lab.patientunitstayid
+    , lab.labresultoffset
+    , min(case when labname = 'paO2' then labresult else null end) as PaO2
+    , min(case when labname = 'FiO2' then
+            -- input validation
+            case
+              when labresult < 0.209 then null
+              when labresult <= 1.0 then labresult*100.0
+              when labresult < 20.9 then null
+              when labresult > 100 then null
+            else labresult end
+          else null end) as FiO2
+  from lab
+  where labname in ('paO2','FiO2')
+  and labresult > 0
+  group by lab.patientunitstayid, lab.labresultoffset
+)
+select
+  t1.patientunitstayid
+  , t1.labresultoffset as pfoffset
+  , t1.pao2
+  , t1.fio2
+  , round(pf.pao2 / pf.fio2 * 100.0,2) as pao2fio2
+from t1
+order by t1.patientunitstayid, t1.labresultoffset;
