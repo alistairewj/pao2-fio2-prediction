1996f61b88b075ceb74b80f4c8ed2a79a637f9ba
theonesp <ma.armengol.hcsc@gmail.com>
2018-04-24 16:46:51 -0400

    v2 tables updated

--- sql/vent-data.sql
+++ sql/vent-data.sql
@@ -1,13 +1,12 @@
-DROP TABLE IF EXISTS pf_vent_data CASCADE;
-CREATE TABLE pf_vent_data AS
+DROP TABLE IF EXISTS public.pf_vent_data_v2 CASCADE;
+CREATE TABLE public.pf_vent_data_v2 AS
 with vent_stg AS
 (  SELECT
-    patientunitstayid
-    , chartoffset
-
+    patientunitstayid, 
+    chartoffset,
 peep , CASE WHEN peep IS NULL THEN 0 ELSE 1 END AS peep_null , 
 SUM(CASE WHEN peep IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY patientunitstayid ORDER BY chartoffset) AS peep_partition
-FROM vent_unpivot_rc
+FROM public.vent_unpivot_rc
 ),
 ven AS(
 SELECT
@@ -19,19 +18,16 @@ FIRST_VALUE(peep) OVER (PARTITION BY patientunitstayid, peep_partition ORDER BY 
 SELECT DISTINCT
     pf.patientunitstayid
   , pf.pfoffset
-
   , pf.pao2
   , pf.fio2
   , pf.pao2fio2
-
-  , LAST_VALUE(t.peep) OVER (
+  , LAST_VALUE(peep) OVER (
       partition by pf.patientunitstayid, pf.pfoffset
-      order by t.chartoffset
+      order by chartoffset
       ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
   ) AS peep
-
 -- source from our "base" cohort
-from pf_pao2fio2 pf
+from public.pf_pao2fio2_v2 pf
 -- now left join to the lab data
 LEFT join ven
   on  pf.patientunitstayid = ven.patientunitstayid
